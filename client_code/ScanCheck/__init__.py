006ab340bc2cb30502efa179799f3ab3835d7bdd
Developer <dev@example.com>
2023-05-30 21:12:04 +0000

    Sorta final 2 scan

--- client_code/ScanCheck/__init__.py
+++ client_code/ScanCheck/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ScanCheckTemplate
+from ._anvil_designer import ScanCheckTemplate as sc
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -6,16 +6,13 @@ from anvil.tables import app_tables
 import anvil.server
 import re
 
-class ScanCheck(ScanCheckTemplate):
+class ScanCheck(sc):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     
     # Any code you write here will run before the form opens.
-  
-  def scan_one_label_show(self, **event_args):
-    """This method is called when the Label is shown on the screen"""
-    self.text_box_1.focus()
+    # globals?
 
   def check_fields_valid(self):
     s1 = self.text_box_1.text
@@ -29,11 +26,11 @@ class ScanCheck(ScanCheckTemplate):
     s1 = self.text_box_1.text
     s2 = self.text_box_2.text
     if not s1:
-      alert("Err: Scan 1 empty")
+      alert("Error: Scan 1 empty")
       self.text_box_1.focus()
       return False
     elif not s2:
-      alert("Err: Scan 2 empty")
+      alert("Error: Scan 2 empty")
       # focus 2
       self.text_box_2.focus()
       return False
@@ -45,6 +42,10 @@ class ScanCheck(ScanCheckTemplate):
     self.text_box_1.text = ""
     self.text_box_2.text = ""
     self.text_box_1.focus()
+    
+  def scan_one_label_show(self, **event_args):
+    """This method is called when the Label is shown on the screen"""
+    self.text_box_1.focus()
 
   def button_reset_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -52,10 +53,6 @@ class ScanCheck(ScanCheckTemplate):
     if c:
       self.clear_page()
       self.text_box_1.focus()
-
-  def text_box_2_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    self.button_compare_click()
   
   def button_compare_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -68,6 +65,8 @@ class ScanCheck(ScanCheckTemplate):
       self.clear_page()
     
   def compare_scans(self):
+    s1 = self.text_box_1.text
+    s2 = self.text_box_2.text
     if not self.check_fields_populated():
       alert("Missing Information")
       return 'missing_info'
@@ -75,12 +74,12 @@ class ScanCheck(ScanCheckTemplate):
       alert("Invalid Barcodes scanned, please verify")
       return 'invalid_info'
     else:
-      s1 = self.extract_pn(self.text_box_1.text)
-      s2 = self.extract_pn(self.text_box_2.text)
-      if s1 == s2:
-        result = f'match {s1} and {s2}'
+      s_extract_1 = self.extract_pn(s1)
+      s_extract_2 = self.extract_pn(s2)
+      if s_extract_1 == s_extract_2:
+        result = f'match {s_extract_1} and {s_extract_2}'
       else:
-        result = f'no_match {s1} and {s2}'
+        result = f'no_match {s_extract_1} and {s_extract_2}'
       alert(result)
       return result
 
@@ -96,3 +95,8 @@ class ScanCheck(ScanCheckTemplate):
   def text_box_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     self.button_compare_click()
+    
+  def text_box_2_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    self.button_compare_click()
+
