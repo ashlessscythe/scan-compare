0e4021fed2a538941fda2dfc3a9a142dc136f583
Developer <dev@example.com>
2023-06-06 15:56:41 +0000

    Edited ScanCheck

--- client_code/ScanCheck/__init__.py
+++ client_code/ScanCheck/__init__.py
@@ -5,7 +5,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
-import regex as re
+import re
 from ..import func
 from .. import globals
 from ..StartNewScanPopup import StartNewScanPopup
